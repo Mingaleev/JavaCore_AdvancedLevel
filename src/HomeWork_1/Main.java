3d843f71039999df33763b52fed5a9a82560eac6
Mingaleev <59661618+Mingaleev@users.noreply.github.com>
2020-11-09 18:30:53 +0300

    Revert "HW1"
    This reverts commit 29e81cf1861958a4dcd4e2f73e55456146d13d6c.

--- src/HomeWork_1/Main.java
+++ src/HomeWork_1/Main.java
@@ -3,28 +3,5 @@ package HomeWork_1;
 public class Main {
     public static void main(String[] args) {
 
-        Running[] running = {
-                new Human("Artur",200,2),
-                new Cat("Murzik",500,3),
-                new Robot("K500",1000,4)
-        };
-
-        Track[] tournament = {
-                new Track(100,0),
-                new Track(200,1),
-                new Track(600,4)
-        };
-
-
-        for (int j = 0; j < tournament.length; j++) {
-            for (int i = 0; i < running.length; i++) {
-                System.out.print("Track №" + (j+1) + " ");
-                tournament[j].start(running[i]);
-            }
-        }
-
-
-
-
     }
 }
